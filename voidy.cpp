663fddf24018a4d5da5d1c03d35da56cdf4e3a16
sleepyyvoidheart <131788101+smaulik17@users.noreply.github.com>
2024-12-10 10:52:07 +0500

    Update voidy.cpp

--- voidy.cpp
+++ voidy.cpp
@@ -1,12 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <ctime>
-#include <cstdlib>
-using namespace std;
-
-const int SIZE=4;
-
 
 bool canMove(int emptyRow,int emptyCol,int tileRow,int tileCol){
     return(abs(emptyRow-tileRow)+abs(emptyCol-tileCol)==1);
